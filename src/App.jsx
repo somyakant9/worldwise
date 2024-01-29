b03edd7c582db0153ffb3054b38d4d21aa9c480a
somyakant9 <msomya143@gmail.com>
2024-01-29 22:13:59 +0530

    added context api structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,13 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 
-const BASE_URL = "http://localhost:8000";
+
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        console.log(data);
-        setCities(data);
-       
-      } catch (err) {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
+ 
 
   return (
     <Routes>
@@ -43,9 +22,9 @@ function App() {
       <Route path="login" element={<Login />} />
       <Route path="app" element={<AppLayout />}>
         <Route index element={<Navigate replace to="cities" />} />
-        <Route path="cities" element={<CityList isLoading={isLoading} cities={cities} />} />
+        <Route path="cities" element={<CityList />} />
         <Route path="cities/:id" element={<City/>}/>
-        <Route path="countries" element={<CountryList isLoading={isLoading} cities={cities}/>} />
+        <Route path="countries" element={<CountryList/>} />
         <Route path="form" element={<Form/>} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
+import { CitiesProvider } from './contexts/CitiesContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CitiesProvider>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </CitiesProvider>
   </React.StrictMode>,
 )
